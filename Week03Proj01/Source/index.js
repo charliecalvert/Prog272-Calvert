9994b37c6f088fca356d88ac8f3040db2069599f
Charles Calvert <java@elvenware.com>
2014-01-21 17:06:58 -0800

    Added get eight

--- Week03Proj01/Source/index.js
+++ Week03Proj01/Source/index.js
@@ -5,3 +5,7 @@
 function getNine() {
 	return 9;
 }
+
+function getEight() {
+	return 8;
+}
--- Week03Proj01/Tests/Test01.js
+++ Week03Proj01/Tests/Test01.js
@@ -4,5 +4,8 @@ describe("Elvenware Jasmine One Suite", function() {
     expect(getNine()).toBe(9);
   });
   
+  it("expect to get nine", function() {
+    expect(getEight()).toBe(8);
+  });
  
 });
